8cde0d589e8b7dc7b5f0101dae7e942d6ee451c4
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-12-02 15:08:39 +0100

    Milestone 3 done, I need to fix layout

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,40 @@
 <template>
-    <div>
-        <div class="ls-card">
-            <div class="ls-card__image">
-               <img :src="image" :alt="title">
+
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img :src="image" :alt="title">
             </div>
-            <div class="ls-card__hover"></div>
-        </div>
-    </div> 
+            <div class="flip-card-back">
+            <p>{{title}}</p> 
+            <p>{{originalTitle}}</p> 
+            <p>Lingua: <img class="flag" :alt="originalLanguage" :src="'http://purecatamphetamine.github.io/country-flag-icons/3x2/' + originalLanguage + `.svg`"></p>
+            <p v-if="voteAverage == 1">Voto: 
+                <span><b-icon icon="star-fill" font-scale="1"></b-icon></span>
+                <span v-for="index in 4" :key="index"><b-icon icon="star" font-scale="1"></b-icon></span>
+            </p>
+            <p v-else-if="voteAverage == 2">Voto: 
+                <span v-for="index in 2" :key="index"><b-icon icon="star-fill" font-scale="1"></b-icon></span>
+                <span v-for="index in 3" :key="index"><b-icon icon="star" font-scale="1"></b-icon></span>
+            </p>
+            <p v-else-if="voteAverage == 3">Voto: 
+                <span v-for="index in 3" :key="index"><b-icon icon="star-fill" font-scale="1"></b-icon></span>
+                <span v-for="index in 2" :key="index"><b-icon icon="star" font-scale="1"></b-icon></span>
+            </p>
+            <p v-else-if="voteAverage == 4">Voto: 
+                <span v-for="index in 4" :key="index"><b-icon icon="star-fill" font-scale="1"></b-icon></span>
+                <span><b-icon icon="star" font-scale="1"></b-icon></span>
+            </p>
+            <p v-else-if="voteAverage == 5">Voto: 
+                <span v-for="index in 5" :key="index"><b-icon icon="star-fill" font-scale="1"></b-icon></span>
+            </p>
+            <p v-else>
+                <span v-for="index in 5" :key="index"><b-icon icon="star" font-scale="1"></b-icon></span>
+            </p>
+            </div>
+         </div>
+    </div>
+  
 </template>
 
 <script>
@@ -15,12 +43,63 @@ export default {
     props: {
         title: String,
         image: String,
+        originalTitle: String,
+        originalLanguage: String,
+        voteAverage: Number,
     },
 
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~bootstrap/dist/css/bootstrap.css';
+@import '~bootstrap-vue/dist/bootstrap-vue.css';
 
+.flip-card {
+  background-color: transparent;
+  width: 200px;
+  height: 270px;
+  perspective: 1000px;
+  display: inline-block;
+  padding: 2px;
+  white-space: pre-wrap;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+  img {
+      width: 100%;
+      height: 100%;
+  }
+}
+
+.flip-card-back {
+  background-color: #2980b9;
+  color: white;
+  transform: rotateY(180deg);
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,23 +8,35 @@
                     <h4>Titolo Originale: {{film.original_title}}</h4>
                     <h4>Lingua: <img class="flag" :alt="film.original_language" :src="'http://purecatamphetamine.github.io/country-flag-icons/3x2/' + stampFlag(film.original_language) + `.svg`">
                     </h4>
-                    <h4>Voto: {{film.vote_average}}</h4> -->
+                    <h4>Voto: {{film.vote_average}}</h4> v-for="film in films" :key="film.id                         :image="`${imageUrl}${film.poster_path}`"
+                        :title="film.title"
+                        :originalTitle="film.original_title"
+                        :voteAverage="convertVote(film.vote_average)"-->
                     <Card v-for="film in films" :key="film.id"
                         :image="`${imageUrl}${film.poster_path}`"
                         :title="film.title"
+                        :originalTitle="film.original_title"
+                        :originalLanguage="convertLanguage(film.original_language)"
+                        :voteAverage="convertVote(film.vote_average)"
                     />
                 </div>
                 <h2>Serie Tv</h2>
-                <div v-for="series in tvSeries" :key="series.id" class="container__card">
-                    <h3>Titolo: {{series.name}}</h3>
+                <div class="container__card">
+                    <!-- <h3>Titolo: {{series.name}}</h3>
                     <h4>Titolo Originale: {{series.original_name}}</h4>
                     <h4>Lingua: <img class="flag" :alt="series.original_language" :src="'http://purecatamphetamine.github.io/country-flag-icons/3x2/' + stampFlag(series.original_language) + `.svg`">
                     </h4>
-                    <h4>Voto: {{convertVote(series.vote_average)}}</h4>
-                    <!-- <Card v-for="series in tvSeries" :key="series.id"
+                    <h4>Voto: {{convertVote(series.vote_average)}}</h4> v-for="series in tvSeries" :key="series.id"                         :image="`${imageUrl}${series.poster_path}`"
+                        :title="series.name"
+                        :originalTitle="series.original_name"
+                        :voteAverage="convertVote(series.vote_average)"-->
+                    <Card v-for="series in tvSeries" :key="series.id"
                         :image="`${imageUrl}${series.poster_path}`"
                         :title="series.name"
-                    /> -->
+                        :originalTitle="series.original_name"
+                        :originalLanguage="convertLanguage(series.original_language)"
+                        :voteAverage="convertVote(series.vote_average)"
+                    />
                 </div>
             </div>
         </main>
@@ -56,7 +68,7 @@ export default {
 
     },
     methods: {
-        stampFlag(language) {
+        convertLanguage(language) {
             if (language === 'en'){
                 return 'GB';
             }
@@ -65,6 +77,12 @@ export default {
             }
             if (language === 'ko'){
                 return 'KR';
+            }
+            if (language === 'zh'){
+                return 'CN';
+            }
+            if (language === 'hi'){
+                return 'IN';
             }
             return language.toUpperCase();
         },
@@ -99,8 +117,9 @@ main {
 
 .container__card {
     margin: 25px auto;
-    display: flex;
-    overflow-y: auto;
+    white-space: nowrap;
+    overflow-x: auto;
+    overflow-y: hidden;
 }
 
 </style>
